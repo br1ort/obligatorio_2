8f5fd1c4dad17af7d587328287b9f758db4ad5d1
br1d2 <brunodorta2002@gmail.com>
2025-10-29 20:20:56 -0300

    Initial commit

---
+++ src/obligatorio_2/run.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package obligatorio_2;
+
+/**
+ *
+ * @author Br1
+ */
+public class run {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
